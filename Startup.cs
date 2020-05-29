8ddabef069e5e7930eede970f749c533c6714954
Cristina <szakacscristina94@yahoo.com>
2020-05-29 15:52:32 +0300

    This is a temporary commit

--- Startup.cs
+++ Startup.cs
@@ -73,6 +73,11 @@ namespace WebApplication2
                 var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
                 c.IncludeXmlComments(xmlPath);
             });
+
+            services.AddSpaStaticFiles(configuration =>
+            {
+                configuration.RootPath = "wwwroot/dist";
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -98,9 +103,24 @@ namespace WebApplication2
 
             app.UseAuthorization();
 
+            app.UseSpaStaticFiles();
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            app.UseSpa(spa =>
+            {
+                // To learn more about options for serving an Angular SPA from ASP.NET Core,
+                // see https://go.microsoft.com/fwlink/?linkid=864501
+
+                spa.Options.SourcePath = "wwwroot";
+
+                //  if (env.IsDevelopment())
+                //  {
+                //  spa.UseAngularCliServer(npmScript: "start");
+                //  }
             });
         }
     }
